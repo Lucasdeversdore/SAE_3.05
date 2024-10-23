a5a455abd7629a634040ae10355433d4855461e9
Devers--Dore Lucas <lucas.devers-dore@etu.univ-orleans.fr>
2024-10-23 09:46:17 +0200

    creation de la table CHIMISTE

---
+++ script.sql
@@ -0,0 +1,10 @@
+create table CHIMISTE (
+    idChimiste int PRIMARY KEY,
+    prenom VARCHAR(50),
+    nom VARCHAR(50),
+    email VARCHAR(50),
+    mdp VARCHAR(50),
+    estPreparateur boolean
+);
+
+
