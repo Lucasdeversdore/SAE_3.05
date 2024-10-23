e283be4617e79e8c3f3c22e0e962c18407172a89
pierre-gangneux <pierre.gangneux@etu.univ-orleans.fr>
2024-10-23 12:59:11 +0200

    ajout html du popup information d'un produit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+# pour tester html , à enlever
+
+class produit:
+    def __init__(self, nom, fonction, stockage, disponible, img):
+        self.nom = nom
+        self.fonction = fonction
+        self.stockage = stockage
+        self.disponible = disponible
+        self.img = img
