3633840cb19bd1f1e161e37ea74865d0875c0925
Devers--Dore Lucas <lucas.devers-dore@etu.univ-orleans.fr>
2024-10-25 09:54:24 +0200

    creation du trigger pour verifier le mot de passe

--- script.sql
+++ script.sql
@@ -94,3 +94,29 @@ CREATE TABLE UNITE (
     nomUnite VARCHAR(50),
     CONSTRAINT PK_Unite PRIMARY KEY (nomUnite)
 );
+
+delimiter |
+CREATE OR REPLACE verifMDP BEFORE INSERT OR UPDATE OF mdp ON CHIMISTE FOR EACH ROW DECLARE 
+DECLARE
+    invalid_password EXCEPTION;
+BEGIN
+    -- Vérifie la présence d'une majuscule
+    IF NOT REGEXP_LIKE(:NEW.mdp, '[A-Z]') THEN
+        RAISE invalid_password;
+    END IF;
+
+    -- Vérifie la présence d'un chiffre
+    IF NOT REGEXP_LIKE(:NEW.mdp, '[0-9]') THEN
+        RAISE invalid_password;
+    END IF;
+
+    -- Vérifie la présence d'un caractère spécial
+    IF NOT REGEXP_LIKE(:NEW.mdp, '[^a-zA-Z0-9]') THEN
+        RAISE invalid_password;
+    END IF;
+
+EXCEPTION
+    WHEN invalid_password THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Le mot de passe doit contenir au moins une majuscule, un chiffre et un caractère spécial.');
+END;
+delimiter ;
