81b9cddf83418a59636ab8d9230b274b88ad4c5b
Tony <tony.kerguen@etu.univ-orleans.fr>
2025-02-24 15:37:24 +0100

    style de la popup cacher produit

--- app/static/app.js
+++ app/static/app.js
@@ -1110,40 +1110,47 @@ function handleButtonDeleteReservation(idCommande, idChimiste) {
 
 // Fonction pour afficher la popup de cacher un produit
 function handleButtonCacherProduit(produit, nomProduit) {
+    // Crée le fond du popup
     const popup_overlay_cacher = document.createElement("div");
     popup_overlay_cacher.id = "popup-overlay-cacher";
 
+    // Contenu du popup
+    const popup_content = document.createElement("div");
+    popup_content.classList.add("popup-content");
+
+    // Titre du popup
     const h3 = document.createElement("h3");
     h3.textContent = `Souhaitez vous cacher: ${nomProduit} ?`;
+    popup_content.appendChild(h3);
 
     // Bouton Non pour fermer le popup
     const bNon = document.createElement("button");
+    const spanNon = document.createElement("span");
+    spanNon.textContent = "Non";
     bNon.className = "cssbuttons-io"
-    bNon.textContent = "Non";
-    bNon.id = "non"; // Associez un ID pour le bouton
     bNon.addEventListener("click", handleButtonNonSuppClick);
-
+    bNon.appendChild(spanNon)
+    
 
     // Bouton Oui 
     const bOui = document.createElement("button");
+    const spanOui = document.createElement("span");
+    spanOui.textContent = "Oui";
     bOui.className = "cssbuttons-io"
-    bOui.textContent = "Oui";
-    bOui.id = "oui"; // Associez un ID pour le bouton
     bOui.data = produit
     bOui.addEventListener("click", function (){
         cacherProduit(produit)
     });
-    // Contenu du popup
-    const popup_content = document.createElement("div");
-    popup_content.style.backgroundColor = "#fff";
-    popup_content.style.padding = "20px";
-    popup_content.style.borderRadius = "5px";
-    popup_content.style.width = "300px";
-    popup_content.style.textAlign = "center";
+    bOui.appendChild(spanOui)
 
-    popup_content.appendChild(h3);
-    popup_content.appendChild(bNon);
-    popup_content.appendChild(bOui);
+    // ligne de bouton
+    const ligne_bouton = document.createElement("div");
+    ligne_bouton.appendChild(bOui)
+    ligne_bouton.appendChild(bNon)
+    ligne_bouton.id = "bouton_modif"
+
+    popup_content.appendChild(ligne_bouton);
+
     popup_overlay_cacher.appendChild(popup_content);
     document.body.appendChild(popup_overlay_cacher); 
 
