126473beb2f7552d2ee87bed933cd88cba87c906
baptiste-mignan <baptiste.mignan@gmail.com>
2025-02-24 15:05:30 +0100

    page settings affichage mdp

--- app/static/app.js
+++ app/static/app.js
@@ -1255,11 +1255,8 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function hideShowPassword(elem){
-    let input = elem.querySelector('#mdp');
-    if (!input){
-        input = elem.querySelector('#confirm_mdp')
-    }
-    let img = elem.querySelector('#toggle');
+    let input = elem.querySelector('input');
+    let img = elem.querySelector('.toggle');
     if (input.type == "password"){
         input.type = "text"
         img.src = "/static/images/afficher.png"
@@ -1269,12 +1266,19 @@ function hideShowPassword(elem){
         img.src = "/static/images/cacher.png"
     }
 }
+
 let password = document.getElementById('pwd')
 if (password){
-    password.querySelector('#toggle').onclick = () => hideShowPassword(password);
+    console.log(password);
+    password.querySelector('.toggle').onclick = () => hideShowPassword(password);
 }
 
 let passwordConf = document.getElementById('pwdConf')
 if (passwordConf){
-    passwordConf.querySelector('#toggle').onclick = () => hideShowPassword(passwordConf);
+    passwordConf.querySelector('.toggle').onclick = () => hideShowPassword(passwordConf);
+}
+
+let passwordNew = document.getElementById('pwdNew')
+if (passwordNew){
+    passwordNew.querySelector('.toggle').onclick = () => hideShowPassword(passwordNew);
 }
